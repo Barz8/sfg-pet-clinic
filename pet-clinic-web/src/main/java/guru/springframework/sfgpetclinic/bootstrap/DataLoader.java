c978c5014f94fe08db06ac17a062a1dda88e7c57
Barz8 <a.longhi@live.it>
2019-04-12 13:33:52 +0200

    Added Project Lombok to project, closes #56

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -62,6 +62,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("123123123");
 
+//        Owner.builder().address("ASDF").id(3L).firstName().build();
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
